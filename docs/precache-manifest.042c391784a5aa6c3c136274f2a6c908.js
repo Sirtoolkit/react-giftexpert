36c72e0e4995793a3ba490fd48dc531e2deb2b5a
Cesar Posada <cesarleonardo537@gmail.com>
2021-05-11 18:43:17 -0600

    new: rename build to docs

---
+++ docs/precache-manifest.042c391784a5aa6c3c136274f2a6c908.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "252fe6ed987da501d0fc",
+    "url": "/static/css/main.22960679.chunk.css"
+  },
+  {
+    "revision": "252fe6ed987da501d0fc",
+    "url": "/static/js/main.2ac488e3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a3b96ec00ff31fcdacba",
+    "url": "/static/js/2.846a0b58.chunk.js"
+  },
+  {
+    "revision": "10f4a243582c96fa951c28b48a094fd2",
+    "url": "/index.html"
+  }
+];
